d3ca121460e78338c7a28354b24042308ad42fcc
Darryl Adams <144128748+DrlDms@users.noreply.github.com>
2026-02-10 11:09:36 +0100

    opdracht 1.1-1.4

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/* Darryl Adams
+ * 1911838
+ */
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, greenhouse/climate monitor!\n";
+    // This is C++ code.
+}
+
+// De semi-colon weghalen op lijn 11 geeft: error: expected ';' before '}' token
+// De accolade op lijn 13 weghalen geeft: error: expected '}' at end of input
+// int a = "number"; geeft error: invalid conversion from 'const char*' to 'int'
